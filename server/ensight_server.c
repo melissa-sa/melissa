feb314339a29b0710d94636b099696ca4c5a28c5
Theophile_Terraz <theophile.terraz@inria.fr>
2017-02-22 17:06:00 +0100

    update ensight server

--- server/ensight_server.c
+++ server/ensight_server.c
@@ -10,6 +10,10 @@
 #include <zmq.h>
 #endif // BUILD_WITH_ZMQ
 #include "server.h"
+#include "melissa_io.h"
+#include "compute_stats.h"
+#include "melissa_options.h"
+#include "melissa_data.h"
 
 static volatile int end_signal = 0;
 
@@ -26,11 +30,11 @@ int main (int argc, char **argv)
     comm_data_t         comm_data;
     int                 time_step;
     int                 i, j;
-    char               *buffer;
+    double             *buffer;
     double            **buff_tab_ptr;
     int                 iteration = 0, nb_iterations = 0;
-    const int           nb_fields = 2;
-    char                field_name[2][255];
+    const int           nb_fields = 1;
+    char                field_name[1][255];
     int                *local_vect_sizes;
     pull_data_t         pull_data;
     char               *field_name_ptr;
@@ -60,6 +64,11 @@ int main (int argc, char **argv)
     comm_data.comm_size       = 1;
     comm_data.rank            = 0;
 #endif // BUILD_WITH_MPI
+    comm_data.client_comm_size = 1;
+    comm_data.rcounts = malloc (sizeof(int));
+    comm_data.rcounts[0] = 1;
+    comm_data.rdispls = malloc (sizeof(int));
+    comm_data.rdispls[0] = 0;
 
 #ifdef BUILD_WITH_PROBES
     start_time = melissa_get_time();
@@ -81,20 +90,33 @@ int main (int argc, char **argv)
 
     melissa_options.global_vect_size = 10;
 
-    local_vect_sizes = melissa_malloc (comm_data.comm_size * sizeof(int));
+    local_vect_sizes = malloc (comm_data.comm_size * sizeof(int));
     for (i=0; i<comm_data.comm_size; i++)
     {
         local_vect_sizes[i] = melissa_options.global_vect_size / comm_data.comm_size;
         if (i < melissa_options.global_vect_size % comm_data.comm_size)
             local_vect_sizes[i] += 1;
     }
-    buffer = melissa_malloc (local_vect_sizes[comm_data.rank] * sizeof(double));
+    buffer = malloc (local_vect_sizes[comm_data.rank] * sizeof(double));
+
+    if (melissa_options.sobol_op == 1)
+    {
+        buff_tab_ptr = malloc ((melissa_options.nb_parameters + 2) * sizeof(double*));
+        for (i=0; i<melissa_options.nb_parameters+2; i++)
+        {
+            buff_tab_ptr[i] = malloc (local_vect_sizes[comm_data.rank] * sizeof(double));
+        }
+    }
+    else
+    {
+        buff_tab_ptr = malloc (sizeof(double*));
+    }
 
     nb_iterations = melissa_options.nb_groups * melissa_options.nb_time_steps ;
-    sprintf(field_name[0], "HeatC");
-    sprintf(field_name[1], "scalar1");
+    sprintf(field_name[0], "scalar1");
     for (i=0; i<nb_fields; i++)
     {
+        fprintf(stdout, "add field %s\n", field_name[i]);
         add_field(&field, field_name[i], 1);
     }
 
@@ -107,24 +129,30 @@ int main (int argc, char **argv)
                 for (rank_id=0; rank_id<melissa_options.nb_parameters+2; rank_id++)
                 {
                     field_name_ptr = field_name[i];
+                    fprintf(stdout, " field_name_ptr = %s\n", field_name_ptr);
                     data_ptr = get_data_ptr (field, field_name_ptr);
                     if (data_ptr->is_valid != 1)
                     {
                         melissa_init_data (data_ptr, &melissa_options, local_vect_sizes[comm_data.rank]);
                     }
 
-                    sprintf(file_name, "results.%s.%.*d", field, 5, time_step +1);
+                    sprintf(file_name, "results.%s.%.*d", field_name_ptr, 5, time_step +1);
 #ifdef BUILD_WITH_PROBES
+                    fprintf(stdout, " Read results.%s.%.*d\n", field_name_ptr, 5, time_step +1);
                     start_comm_time = melissa_get_time();
 #endif // BUILD_WITH_PROBES
-                    read_ensight (melissa_options, comm_data, buffer, local_vect_sizes, file_name);
+                    read_ensight (&melissa_options, &comm_data, buffer, local_vect_sizes, file_name);
 #ifdef BUILD_WITH_PROBES
                     end_comm_time = melissa_get_time();
                     total_comm_time += end_comm_time - start_comm_time;
 #endif // BUILD_WITH_PROBES
-                    memcpy(buff_tab_ptr[rank_id], buffer, local_vect_sizes[comm_data.rank] * sizeof(double));
+                    if (melissa_options.sobol_op == 1)
+                    {
+                        memcpy(buff_tab_ptr[rank_id], buffer, local_vect_sizes[comm_data.rank] * sizeof(double));
+                    }
 
 #ifdef BUILD_WITH_PROBES
+                    fprintf(stdout, " compute results...\n");
                     start_computation_time = melissa_get_time();
 #endif // BUILD_WITH_PROBES
 
@@ -144,14 +172,14 @@ int main (int argc, char **argv)
                         //                                                                        melissa_options.nb_time_steps,
                         //                                                                        melissa_options.nb_parameters);
 
+                    }
 #ifdef BUILD_WITH_PROBES
-                        end_computation_time = melissa_get_time();
-                        total_computation_time += end_computation_time - start_computation_time;
+                    end_computation_time = melissa_get_time();
+                    total_computation_time += end_computation_time - start_computation_time;
 #endif // BUILD_WITH_PROBES
-                        if (comm_data.rank==0 && (iteration % 10) == 0 )
-                        {
-                            fprintf(stderr, "iteration %d / %d  - field \"%s\"\n", iteration, nb_iterations*nb_fields, field_name_ptr);
-                        }
+                    if (comm_data.rank==0 && (iteration % 10) == 0 )
+                    {
+                        fprintf(stderr, "iteration %d / %d  - field \"%s\"\n", iteration, nb_iterations*nb_fields, field_name_ptr);
                     }
 
                     if (end_signal != 0)
@@ -219,7 +247,7 @@ int main (int argc, char **argv)
         fprintf (stdout, " --- Writing time:                    %g s\n", total_write_time);
         fprintf (stdout, " --- Total time:                      %g s\n", melissa_get_time() - start_time);
         fprintf (stdout, " --- Stats structures memory:         %ld bytes\n", mem_conso(&melissa_options));
-        fprintf (stdout, " --- Bytes written:                   %ld bytes\n", count_bytes_written(&melissa_options)*nb_fields);
+        fprintf (stdout, " --- Bytes written:                   %ld bytes\n", count_mbytes_written(&melissa_options)*nb_fields);
     }
 #endif // BUILD_WITH_PROBES
 
