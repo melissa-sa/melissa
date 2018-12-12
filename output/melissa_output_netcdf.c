f17635049108e9491984a3e4d67b6024c49608be
friedems <sebastian.friedemann@univ-grenoble-alpes.fr>
2018-12-12 17:11:07 +0100

    Improve netcdf writing by keepinf files open

--- output/melissa_output_netcdf.c
+++ output/melissa_output_netcdf.c
@@ -74,7 +74,7 @@ int getDim(int ncID, const char *name, size_t len, int *idp)
 /**
  * Creates variables or only returns its id if already existent.
  */
-void getVar(int ncID, const char *name, int ndims, const int dimids[], int *idp, nc_type xtype)
+void getVar(const int ncID, const char *name, const int ndims, const int dimids[], int *idp, const nc_type xtype)
 {
     nc_redef(ncID);
     int res = nc_def_var(ncID, name, xtype, ndims, dimids, idp);
@@ -107,13 +107,73 @@ void getDimName(char * dest, int i, int dims)
     }
 }
 
+#define MAX_DIMS 10
+#define MAX_FILE_NAME 512
+typedef struct  {
+    const char file_name[MAX_FILE_NAME];
+    size_t dims;
+    size_t n[MAX_DIMS];
+    int ncID;
+
+
+    int variable_var_id;
+    int time_var_id;
+
+} CacheEntry;
+
+// I want to write CPP!!
+// Allows 512 different variables...
+static CacheEntry cache[512];
+static size_t last_entry = 0;
+
+// Returns NULL if not found.
+CacheEntry * findEntry(const char* file_name)
+{
+    int i;
+    for (i = 0; i < 512; ++i)
+    {
+        if (strcmp(cache[i].file_name, file_name) == 0)
+            return &cache[i];
+    }
+    return NULL;
+}
+
+
 
 /**
  * Opens/ creates file if not already there
  */
-int CreateFile(const char* file_name, size_t dims, size_t n[], int ID[], int *ptime_id)
+CacheEntry * CreateFile(const char* file_name, const char* variable_name, size_t vec_size, nc_type xtype)
 {
-    int ncID = 0;
+    CacheEntry * c = &cache[last_entry];
+    last_entry++;
+
+    // nice value when dimensions turn out to be 0.
+    c->n[0] = vec_size;
+    getDimsFromEnv(&c->dims, c->n);
+    // sometimes we get only 2D slizes... handle them nicely!
+    //printf("\nshape: %d %d %d == %d\n", c->n[0], c->n[1], c->n[2], vec_size);
+    if (vec_size == c->n[1] * c->n[2])
+    {
+        c->dims = 2;
+        c->n[0] = c->n[1];
+        c->n[1] = c->n[2];
+    }
+    else
+    {
+        int prod = 1;
+        int i;
+        for (i = 0; i < c->dims; ++i)
+        {
+            prod *= c->n[i];
+        }
+        if (prod != vec_size)
+        {
+            c->n[0] = vec_size;
+            c->dims=1;
+        }
+    }
+    c->ncID = 0;
 
     if (access(file_name, F_OK) == -1)
     {
@@ -123,28 +183,27 @@ int CreateFile(const char* file_name, size_t dims, size_t n[], int ID[], int *pt
         //int status = nc_create_par(file_name, NC_NETCDF4 | NC_MPIIO, comm_data->comm, MPI_INFO_NULL, &ncID);
         //#else
         //int status = nc_create(file_name, NC_NETCDF4, &ncID);
-        int status = nc_create(file_name, 0, &ncID);
+        int status = nc_create(file_name, 0, &c->ncID);
         //#endif
         if (status != NC_NOERR)
         {
             printf("Error! nc error code: %d\c", status);
-            printf("Error creating file!");
             ERROR("Could not create file!");
         }
     }
     else
     {
         // open it
+        // should never get here...
         D("file exists already. Opening it!");
         //#ifdef BUILD_WITH_MPI
         //if (nc_open_par(file_name, NC_MPIIO | NC_WRITE, comm_data->comm, MPI_INFO_NULL, &ncID) != NC_NOERR)
         //#else
-        int status = nc_open(file_name, NC_WRITE, &ncID);
+        int status = nc_open(file_name, NC_WRITE, &c->ncID);
         if (status!= NC_NOERR)
             //#endif
         {
             printf("Error! nc error code: %d", status);
-            printf("Error opening existing file!");
             ERROR("Could not open file!");
         }
     }
@@ -157,17 +216,39 @@ int CreateFile(const char* file_name, size_t dims, size_t n[], int ID[], int *pt
     //getDim(ncID, "z", nZ, pzID);
     //getDim(ncID, "time", NC_UNLIMITED, ptime_id);
 
+    int ID[c->dims];
     int i;
-    for (i = 0; i < dims; ++i)
+    for (i = 0; i < c->dims; ++i)
     {
         char dim_name[256];
-        getDimName(dim_name, i, dims);
-        getDim(ncID, dim_name, n[i], &(ID[i]));
+        getDimName(dim_name, i, c->dims);
+        getDim(c->ncID, dim_name, c->n[i], &ID[i]);
     }
 
-    getDim(ncID, "time", NC_UNLIMITED, ptime_id);
+    int time_id;
 
-    return ncID;
+    // TODO: maybe we are more performant if we write out all the time variables at once!
+    getDim(c->ncID, "time", NC_UNLIMITED, &time_id);
+
+    int variable_dims[c->dims+1];  //={ time_id, zID, yID, xID };
+    variable_dims[0] = time_id;
+    memcpy(&variable_dims[1], ID, sizeof(int) * c->dims);
+
+    getVar(c->ncID, variable_name, c->dims+1, variable_dims, &c->variable_var_id, xtype);
+
+    getVar(c->ncID, "time", 1, &time_id, &c->time_var_id, NC_DOUBLE);
+
+    // set time only once per file!
+    //nc_var_par_access(current_file_id, time_var_id, NC_COLLECTIVE);
+    // TODO: check if already something in time variable!
+    //size_t len_t;
+    //nc_inq_dimlen(current_file_id, time_var_id, &len_t);
+    // required for compatibility with netcdf3 :
+    nc_enddef(c->ncID);
+
+    strcpy(c->file_name, file_name);
+
+    return c;
 }
 
 void write_netcdf(const char   *statistics_name,
@@ -177,38 +258,6 @@ void write_netcdf(const char   *statistics_name,
         const void * vec[],
         const nc_type xtype)
 {
-    // TODO: keep files open over all timesteps!
-#define MAX_DIMS 10
-
-    // nice value when dimensions turn out to be 0.
-    size_t n[MAX_DIMS];
-    n[0] = vec_size;
-    size_t dims;
-    getDimsFromEnv(&dims, n);
-    // sometimes we get only 2D slizes... handle them nicely!
-    printf("\nshape: %d %d %d == %d\n", n[0], n[1], n[2], vec_size);
-    if (vec_size == n[1] * n[2])
-    {
-        printf("2D");
-        dims = 2;
-        n[0] = n[1];
-        n[1] = n[2];
-    }
-    else
-    {
-        int prod = 1;
-        int i;
-        for (i = 0; i < dims; ++i)
-        {
-            prod *= n[i];
-        }
-        if (prod != vec_size)
-        {
-            printf("1D");
-            n[0] = vec_size;
-            dims=1;
-        }
-    }
 
             // just write vector
 
@@ -222,61 +271,63 @@ void write_netcdf(const char   *statistics_name,
 
     // the following variables are currently just used by rank 0
 
-    int current_file_id;
-    int ID[dims];
-    int time_id;
-
-    int variable_var_id;
-    int time_var_id;
-
     int nc_status;
 
     char file_name[256];
 
     sprintf(file_name, "%s_%s.nc", variable_name, statistics_name);
 
-    current_file_id = CreateFile(file_name, dims, n, ID, &time_id);
+    CacheEntry *c;
+    if (t == 0)
+    {
+        c = CreateFile(file_name, variable_name, vec_size, xtype);
+    }
+    else
+    {
+        c = findEntry(file_name);
+    }
+
     // add variable Time and variable "variable":
-    int variable_dims[dims+1];  //={ time_id, zID, yID, xID };
-    variable_dims[0] = time_id;
-    memcpy(&variable_dims[1], ID, sizeof(size_t) * dims);
 
-    getVar(current_file_id, variable_name, dims+1, variable_dims, &variable_var_id, xtype);
-
-    getVar(current_file_id, "time", 1, &time_id, &time_var_id, NC_DOUBLE);
-
-    // set time only once per file!
-    //nc_var_par_access(current_file_id, time_var_id, NC_COLLECTIVE);
-    // TODO: check if already something in time variable!
-    //size_t len_t;
-    //nc_inq_dimlen(current_file_id, time_var_id, &len_t);
-    // required for compatibility with netcdf3 :
-    nc_enddef(current_file_id);
-
-    size_t start[dims+1];  //= { t, 0, 0, 0 };
-    size_t count[dims+1];  //= { 1, nZ, nY, nX};
+    size_t start[c->dims+1];  //= { t, 0, 0, 0 };
+    size_t count[c->dims+1];  //= { 1, nZ, nY, nX};
     start[0] = t;
     count[0] = 1;  // writing one value
     double double_time = (double) t;
-    nc_status = nc_put_vara_double(current_file_id, time_var_id, start, count, &double_time);
-
+    nc_status = nc_put_vara_double(c->ncID, c->time_var_id, start, count, &double_time);
 
     // refresh all the values of time step t:
     //                  t  z  y  x
     start[0] = t;
-    for (i = 1; i < dims+1; ++i)
+    for (i = 1; i < c->dims+1; ++i)
     {
         start[i] = 0;
     }
     count[0] = 1;
-    memcpy(&count[1], n, sizeof(size_t) * dims);
-    printf("%d,%d,%d,%d", count[0], count[1], count[2], count[3]);
+    memcpy(&count[1], c->n, sizeof(size_t) * c->dims);
+    //printf("%d,%d,%d,%d", count[0], count[1], count[2], count[3]);
     // now do a write to the cdf! (! all the preparations up there are necessary!)
-    nc_status = nc_put_vara_double(current_file_id, variable_var_id, start, count, vec);
+    nc_status = nc_put_vara_double(c->ncID, c->variable_var_id, start, count, vec);
     //printf("put_state: %d\n", nc_status);
     D("putting doubles");
-    nc_close(current_file_id);
     D("wrote %s\n", file_name);
+
+    const char * cnb_timesteps = getenv("MELISSA_NETCDF_NB_TIME_STEPS");
+    if (cnb_timesteps == NULL)
+    {
+        ERROR("Need to set environment variable MELISSA_NETCDF_NB_TIME_STEPS!");
+    }
+
+    int nb_timesteps = atoi(cnb_timesteps);
+    if (nb_timesteps == 0)
+    {
+        ERROR("Need meaningful value (e.g. >0) for MELISSA_NETCDF_NB_TIME_STEPS!");
+    }
+    if (t == nb_timesteps-1)
+    {
+        nc_close(c->ncID);
+        D("Closed file!");
+    }
 }
 
 void write_output_d_netcdf(const char   *file_name,
