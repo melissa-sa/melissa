1744195f4af6c10d5383ad491e2ff04ebeb0ed42
Terraz Theophile <theophile.terraz@inria.fr>
2019-02-13 10:01:33 +0100

    work on learning

--- server/melissa_learning.py
+++ server/melissa_learning.py
@@ -37,7 +37,9 @@ import os.path
 import time
 import random
 import keras
-#import matplotlib.pyplot as plt
+import MEDLoader as ml
+import MEDCoupling as mc
+import matplotlib.pyplot as plt
 #from keras.models import Sequential
 #from keras.layers import Dense, Activation
 #from keras import backend as K
@@ -51,9 +53,12 @@ def rescale_params_fluid(param_set):
 
 def rescale_params_heat(param_set):
 #    param_set[-1] = float(param_set[-1])/100.
+    param_set[-1] = 0.
+#    print " TIMESTEP " + str(param_set[-1])
     return param_set
 
 def rescale_params(param_set):
+#    return rescale_params_heat(param_set)
     return param_set
 
 def rescale_data_fluid(data):
@@ -65,7 +70,53 @@ def rescale_data(data):
     return data
 
 
-def neighbors_square(size):
+def neighbors_MED():
+    indices = []
+    mesh = ml.ReadUMeshFromFile("/home/tterraz/Documents/deep_learning_subashuny/final_output/von_karman/MESH/COARSE/Cylinder_7361.med","Cylinder_1",0)
+    tab1, tab2 = mesh.computeNeighborsOfCells()
+    for i in range(len(tab2)-1): # cell
+        diag_inserted = False
+        for k in tab1[tab2[i]:tab2[i+1]]: # neighbors
+            if k[0] > i and not diag_inserted:
+                indices.append([i,i])
+                diag_inserted = True
+            indices.append([i,k[0]])
+        if not diag_inserted:
+            indices.append([i,i])
+    return indices
+
+def neighbors_MED_csr():
+    indices = []
+    mesh = ml.ReadUMeshFromFile("/home/tterraz/Documents/deep_learning_subashuny/final_output/von_karman/MESH/COARSE/Cylinder_7361.med","Cylinder_1",0)
+    tab1, tab2 = mesh.computeNeighborsOfCells()
+    for i in range(len(tab2)-1): # cell
+        indices.append([])
+        diag_inserted = False
+        for k in tab1[tab2[i]:tab2[i+1]]: # neighbors
+            print "k = "+str(k)
+            if k[0] > i and not diag_inserted:
+                indices[i].append(i)
+                diag_inserted = True
+            indices[i].append(k[0])
+        if not diag_inserted:
+            indices[i].append(i)
+    return indices
+
+def neighbors_read():
+    indices = [[0, 0]]
+    file = open("VKindices", "r")
+    for line in file:
+        indices.append([int(i) for i in line.split()])
+    file.close()
+    return indices
+
+def neighbors_write(indices):
+    file = open("VKindices", "w")
+    for i in indices:
+        file.write(str(i[0])+" "+str(i[1])+"\n")
+    file.close()
+
+def neighbors_square_sparse_tensor(size):
     x = int(np.sqrt(size))
     y = int(np.sqrt(size))
     indices = []
@@ -81,6 +132,25 @@ def neighbors_square(size):
             indices.append([i,i+1])
         if l < y-1:
             indices.append([i,i+x])
+    return indices
+
+def neighbors_square_csr(size):
+    x = int(np.sqrt(size))
+    y = int(np.sqrt(size))
+    indices = []
+    for i in range(size):
+        indices.append([])
+        k = i%x # col
+        l = np.floor(i/x) # row
+        if l > 0:
+            indices[i].append(i-x)
+        if k > 0:
+            indices[i].append(i-1)
+        indices[i].append(i)
+        if k < x-1:
+            indices[i].append(i+1)
+        if l < y-1:
+            indices[i].append(i+x)
     return indices
 
 class MyFirstLayer(tf.keras.layers.Layer):
@@ -130,111 +200,211 @@ class MyFirstLayer(tf.keras.layers.Layer):
         print "output_shape "+str(out.shape)
         return self.act(tf.add(out, self.biases))
 
-class MyMidLayer(tf.keras.layers.Layer):
-    def __init__(self, size, trainable=True):
-        super(MyMidLayer, self).__init__()
-        self.size = size
-#        self.values = self.add_variable("values",
-#                                         shape=[self.in_size, 1, self.out_size],
-#                                         dtype=tf.float32,
-#                                         initializer=tf.keras.initializers.RandomUniform,
-#                                         trainable=True)
+#class MyMidLayer(tf.keras.layers.Layer):
+#    def __init__(self, size, trainable=True):
+#        super(MyMidLayer, self).__init__()
+#        self.size = size
+##        self.values = self.add_variable("values",
+##                                         shape=[self.in_size, 1, self.out_size],
+##                                         dtype=tf.float32,
+##                                         initializer=tf.keras.initializers.RandomUniform,
+##                                         trainable=True)
 
+#        self.biases = self.add_variable("biases",
+#                                        shape=[self.out_size],
+#                                        dtype=tf.float32,
+#                                        initializer=tf.keras.initializers.RandomUniform,
+#                                        #initializer=tf.keras.initializers.Zeros,
+#                                        trainable=True)
+
+#        self.coefs = self.add_variable("coefs",
+#                                       shape=[self.in_size, self.out_size],
+#                                       dtype=tf.float32,
+#                                       initializer=tf.keras.initializers.RandomUniform,
+#                                       #initializer=tf.keras.initializers.Zeros,
+#                                       trainable=True)
+#        self.act = tf.keras.layers.ReLU()
+##        self.conv = tf.keras.layers.Conv1D(out_size, 1, input_shape=[None, in_size, 1])
+
+#    def compute_output_shape(self, input_shape):
+#        return (input_shape[0], self.size)
+
+#    def call(self, input):
+##        res = tf.reshape(input, [self.in_size,1,1])
+##        print res.shape
+##        out = tf.matmul(res, self.values)
+##        print out.shape
+##        out = tf.reshape(out, [self.in_size, self.out_size])
+##        print out.shape
+#        input = tf.expand_dims(input, -1)
+#        print "input_shape "+str(input.shape)
+##        out = self.conv (input)
+#        multiples = [1, 1, 10]
+#        out = tf.multiply(tf.tile (input, multiples), self.coefs)
+#        print out.shape
+#        out = tf.reduce_sum(out, axis = [1])
+#        print "output_shape "+str(out.shape)
+#        return self.act(tf.add(out, self.biases))
+
+
+
+class MySparseLayerSparseTensor(tf.keras.layers.Layer):
+    def __init__(self, size, trainable=True):
+        super(MySparseLayerSparseTensor, self).__init__()
+        self.size = size
+#        self.indices = neighbors_square_sparse_tensor(size)
+        self.indices = neighbors_MED()
+        #print "value shape: " + str(self.sparse_tensor.values.shape)
+        #print "indices: " + str(self.sparse_tensor.indices)
+#        indice_size = self.values_ptr[-1]
+        self.values = self.add_variable("values",
+                                         shape=[len(self.indices)],
+                                         dtype=tf.float32,
+                                         initializer=tf.keras.initializers.RandomUniform,
+                                         trainable=True)
+        self.sparse_tensor = tf.SparseTensor(self.indices, self.values, [self.size, self.size])
         self.biases = self.add_variable("biases",
-                                        shape=[self.out_size],
+                                        shape=[self.size],
                                         dtype=tf.float32,
                                         initializer=tf.keras.initializers.RandomUniform,
-                                        #initializer=tf.keras.initializers.Zeros,
+#                                        initializer=tf.keras.initializers.Zeros,
                                         trainable=True)
-
-        self.coefs = self.add_variable("coefs",
-                                       shape=[self.in_size, self.out_size],
-                                       dtype=tf.float32,
-                                       initializer=tf.keras.initializers.RandomUniform,
-                                       #initializer=tf.keras.initializers.Zeros,
-                                       trainable=True)
-        self.act = tf.keras.layers.ReLU()
-#        self.conv = tf.keras.layers.Conv1D(out_size, 1, input_shape=[None, in_size, 1])
+        self.act = tf.keras.layers.PReLU(alpha_initializer=tf.keras.initializers.RandomUniform)
+#        self.act = tf.keras.activations.linear
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.size)
 
     def call(self, input):
-#        res = tf.reshape(input, [self.in_size,1,1])
-#        print res.shape
-#        out = tf.matmul(res, self.values)
-#        print out.shape
-#        out = tf.reshape(out, [self.in_size, self.out_size])
-#        print out.shape
-        input = tf.expand_dims(input, -1)
-        print "input_shape "+str(input.shape)
-#        out = self.conv (input)
-        multiples = [1, 1, 10]
-        out = tf.multiply(tf.tile (input, multiples), self.coefs)
-        print out.shape
-        out = tf.reduce_sum(out, axis = [1])
-        print "output_shape "+str(out.shape)
-        return self.act(tf.add(out, self.biases))
+        return self.act(tf.nn.bias_add(tf.linalg.transpose(tf.sparse_tensor_dense_matmul(self.sparse_tensor, input, adjoint_b = True)), self.biases))
+#        return tf.linalg.transpose(tf.sparse_tensor_dense_matmul(self.sparse_tensor, input, adjoint_b = True))
 
-class MySparseLayer(tf.keras.layers.Layer):
+class MySparseLayerCSR(tf.keras.layers.Layer):
     def __init__(self, size, trainable=True):
-        super(MySparseLayer, self).__init__()
+        super(MySparseLayerCSR, self).__init__()
         self.size = size
-#        self.indices = neighbors_square(size)
-        #self.indices = reighbors_read()
+        self.indices = neighbors_square_csr(size)
+#        self.indices = neighbors_MED_csr()
+        self.values_ptr = [0]
+        for temp in self.indices:
+            self.values_ptr.append(len(temp)+self.values_ptr[-1])
+        self.partitions = np.zeros(self.values_ptr[-1])
+        for i in range(self.size):
+            self.partitions[self.values_ptr[i]:self.values_ptr[i+1]]=i
         #print "value shape: " + str(self.sparse_tensor.values.shape)
         #print "indices: " + str(self.sparse_tensor.indices)
-        indice_size = len(self.indices)
+#        indice_size = self.values_ptr[-1]
         self.values = self.add_variable("values",
-                                         shape=[len(self.indices)],
+                                         shape=[self.values_ptr[-1]],
                                          dtype=tf.float32,
-                                         initializer=tf.keras.initializers.Ones,
-                                         trainable=False)
-        self.sparse_tensor = tf.SparseTensor(self.indices, self.values, [self.size, self.size])
-#        self.coefs = self.add_variable("coefs",
-#                                        shape=[self.size],
-#                                        dtype=tf.float32,
-#                                        initializer=tf.keras.initializers.RandomUniform,
-#                                        trainable=True)
+                                         initializer=tf.keras.initializers.RandomUniform,
+                                         trainable=True)
         self.biases = self.add_variable("biases",
                                         shape=[self.size],
                                         dtype=tf.float32,
-                                        initializer=tf.keras.initializers.RandomUniform,
-                                        #initializer=tf.keras.initializers.Zeros,
+#                                        initializer=tf.keras.initializers.RandomUniform,
+                                        initializer=tf.keras.initializers.Zeros,
                                         trainable=True)
+#        self.output_var = self.add_variable("output_var",
+#                                             shape=[self.size],
+#                                             dtype=tf.float32,
+#                                             initializer=tf.keras.initializers.Zeros,
+#                                             trainable=False)
         self.act = tf.keras.layers.PReLU()
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.size)
 
+    def cond1(self, i, j, input, output):
+        return i < self.size
+
+    def body1(self, i, j, input, output):
+        print "i = " + str(i)
+        print "j = " + str(j)
+#        tensorflow sparse tensor
+#        output[self.indices[i][0]] = tf.math.add (output[self.indices[i][0]], tf.math.multiply(input[self.indices[i][1]], self.values[i]))
+#        csr
+        x = tf.gather(self.values, np.arange(self.values_ptr[j], self.values_ptr[j+1]))
+        y = tf.gather(input, self.indices[j][:])
+        output[i] = tf.tensordot(x, y)
+        print "input[i] = " + str(input[i])
+        return i + 1, j+1,  input, output
+
+    def sparse_vectmat(self, vect):
+        i = tf.constant(0)
+        j = 0
+        output_var = tf.fill([vect.shape[-1]], 0)
+        i, j, vect, output_var = tf.while_loop(self.cond1, self.body1, loop_vars=[i, j, vect, output_var])
+        return output_var
+
+    def sparse_vectmat_old(self, vect):
+        output_var = tf.fill([vect.shape[-1]], 0)
+        for i in range(len(self.indices)):
+            output_var[self.indices[i][0]] = tf.add (output_var[self.indices[i][0]], tf.multiply(vect[self.indices[i][1]], self.values[i]))
+        return output_var
+
+    def sparse_vectmat_test(self, vect):
+#        output_var = np.zeros(vect.shape[-1])
+#        output_var = tf.TensorArray(dtype=tf.float32, size=self.size)
+        output_list = []
+        print "vect = " + str(vect)
+        y_list = tf.dynamic_partition(self.values, self.partitions, self.size)
+        for i in range(self.size):
+            x = tf.gather(vect, self.indices[i], axis = 1)
+#            y = tf.gather(self.values, np.arange(self.values_ptr[i], self.values_ptr[i+1]))
+
+#            y = y_list[i]
+#            print "y shape : "+str(y.shape)
+#            print "y= "+str(y)
+            y = tf.reshape(y_list[i], [len(self.indices[i]), 1])
+#            print "x = " + str(x)
+#            print "y = " + str(y)
+#            print "tensordot = " + str(tf.matmul(x, y))
+#            output_var = output_var.write(i, tf.matmul(x, y))
+            output_list.append(tf.tensordot(x, y, 1))
+#        print "output var = " + str(output_var)
+#        out = output_var.stack()
+        out = tf.concat(output_list, 1)
+#        out = tf.reshape(out, [-1, 100])
+        print "out stack = " + str(out)
+#        out = output_var.concat()
+#        print "out concat = " + str(out)
+#        out = tf.concat(output_var, 0)
+#        print "out = " + str(out)
+        return out
+
+
     def call(self, input):
-#        res = tf.multiply(input, self.coefs)
-        return self.act(tf.nn.bias_add(tf.linalg.transpose(tf.sparse_tensor_dense_matmul(self.sparse_tensor, input, adjoint_b = True)), self.biases))
+        print "input shape ===== :" + str(input.shape)
+#        output_var = tf.fill([-1, self.size], 0)
+#        return self.act(tf.nn.bias_add(tf.linalg.transpose(tf.sparse_tensor_dense_matmul(self.sparse_tensor, input, adjoint_b = True)), self.biases))
+        return self.act(tf.nn.bias_add(self.sparse_vectmat_test(input), self.biases))
+#        return self.sparse_vectmat_test(input)
 
 
 class MyModel(tf.keras.Model):
     def __init__(self, nb_parameters, vect_size):
         super(MyModel, self).__init__()
-        self.first_layer = MyFirstLayer(nb_parameters, vect_size, trainable = True)
-#        self.dense1 = tf.keras.layers.Dense(vect_size,
-#                                            kernel_initializer=tf.keras.initializers.RandomUniform,
-#                                            bias_initializer=tf.keras.initializers.RandomUniform,
-#                                            activation=tf.keras.activations.linear,
-#                                            trainable = True)
+#        self.first_layer = MyFirstLayer(nb_parameters, vect_size, trainable = True)
+        self.dense1 = tf.keras.layers.Dense(vect_size,
+                                            kernel_initializer=tf.keras.initializers.Ones,
+                                            bias_initializer=tf.keras.initializers.Zeros,
+                                            activation=tf.keras.activations.linear,
+                                            trainable = True)
         self.sparse = []
-        for i in range(10):
-            self.sparse.append(MySparseLayer(vect_size, trainable = True))
-        self.out = MySparseLayer(vect_size, trainable = True)
+        for i in range(6):
+            self.sparse.append(MySparseLayerSparseTensor(vect_size, trainable = True))
+        self.out = MySparseLayerSparseTensor(vect_size, trainable = True)
 
-    def call(self, inputs):
-        x = self.first_layer(inputs)
-#        x = self.dense1(x)
-        x_saved = tf.identity(x)
+    def call(self, x):
+#        x = self.first_layer(x)
+        x = self.dense1(x)
+#        x_saved = tf.identity(x)
         for i in range(len(self.sparse)):
             x = self.sparse[i](x)
-            if (i%2 == 0 and i > 0):
-                x = tf.keras.layers.add([x, x_saved])
-                x_saved = tf.identity(x)
+#            if (i%2 == 0 and i > 0):
+#                x = tf.keras.layers.add([x, x_saved])
+#                x_saved = tf.identity(x)
         return self.out(x)
 #        return x
 
@@ -287,9 +457,9 @@ class melissa_helper:
         self.test_x = []
         self.test_y = []
         self.nb_parameters = nb_parameters
-#        self.model = MyModel(nb_parameters, vect_size)
+        self.model = MyModel(nb_parameters, vect_size)
 #        self.model = DenseModel(nb_parameters, vect_size, nb_layers = 5)
-        self.model = InitModelSubashiny(nb_parameters, vect_size)
+#        self.model = InitModelSubashiny(nb_parameters, vect_size)
 
 def model_init_minibatch(vect_size, nb_parameters):
     hvd.init()
