83656c71380f0693de8ecb63bb990c4464f41238
Terraz Theophile <theophile.terraz@inria.fr>
2019-06-05 16:22:59 +0200

    big changes in C API

--- examples/heat_example/src/heat_no_mpi.c
+++ examples/heat_example/src/heat_no_mpi.c
@@ -140,7 +140,7 @@ int main( int argc, char **argv )
 
   // melissa_init_no_mpi is the first Melissa function to call, and it is called only once.
   // It mainly contacts the server.
-  melissa_init_no_mpi(field_name, &vect_size, &simu_id, &coupling);
+  melissa_init_no_mpi(field_name, vect_size, simu_id, coupling);
 
   // main loop:
   for(n=1;n<=nmax;n++)
