408fe69060412d438026874c5c9d3c7e3aa5aeea
Terraz Theophile <theophile.terraz@inria.fr>
2019-09-25 16:37:57 +0200

    add missing script

---
+++ examples/virtual_cluster/oardocker.sh
@@ -0,0 +1,57 @@
+#! /bin/bash -e
+
+# Script to install a oardocker virtual cluster with melissa dependencies installed, oar and slurm batch scehduler.
+
+# # get and install oar-docker
+git clone https://github.com/oar-team/oar-docker.git
+cd oar-docker
+git checkout dev
+python -m pip install -e .
+cd ..
+
+# # init images vith debian stretch
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+oardocker init -e stretch
+
+# Add to  the Dockerfile commands to install   Melissa dependencies and Slurm:
+cp custom_setup.base.sh  .oardocker/images/base/custom_setup.sh
+cp custom_setup.node.sh  .oardocker/images/node/custom_setup.sh
+cp custom_setup.server.sh  .oardocker/images/server/custom_setup.sh
+
+# copy the slurm config file (done in slurm_start.sh)
+# cp slurm.conf .oardocker/images/base/config/slurm.conf
+
+# build the images. must be done each time Dockerfile is modified
+oardocker build
+# install OAR on the cluster
+oardocker install http://oar-ftp.imag.fr/oar/2.5/sources/testing/oar-2.5.8.tar.gz
+
+# start the cluster, with -n <number of nodes>
+oardocker start -n 3 -v $PWD/../../:/home/docker/melissa
+# we can also share a directory:
+# oardocker start -n 4 -v $HOME/mon_super_projet:/data
+
+
+# Start slurm on the virtual cluster
+./slurm_start.sh
+
+
+# Get Melissa from github
+#oardocker exec frontend git clone https://github.com/melissa-sa/melissa.git
+
+# Configure, build, install and test  melissa
+#oardocker exec frontend mkdir melissa/build
+#oardocker exec frontend cd melissa/build && cmake ../source -DBUILD_DOCUMENTATION=OFF -DINSTALL_ZMQ=ON && make install && source ../install/melissa_set_env.sh && ctest
+
+# # Stop the cluster 
+# oardocker stop 
+# # Stop and remove all images (except the base lunix container) 
+# docker destroy 
+
+# Similar but from docker 
+# # Stop the cluster
+# docker stop $(docker ps -a -q)
+# # delete the containers and images
+# docker rm $(docker ps -a -q)
+# docker rmi $(docker images -q)
