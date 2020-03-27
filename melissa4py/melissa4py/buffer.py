26a7556f1617042b08a7d37a4021dd097c1266c8
Achal Agarwal <achalagarwal.01@gmail.com>
2020-03-26 18:52:55 -0700

    Added Cyclic Bucketized Learning Rate Scheduler with Bucketized Buffers

--- melissa4py/melissa4py/buffer.py
+++ melissa4py/melissa4py/buffer.py
@@ -81,3 +81,76 @@ class PartitionedReplayBuffer:
 
 
 
+
+# these replay buffers are in sync with learning rate
+# the rule of thumb is that there will be discrete values of learning rates
+# and the learning rates will be known to the buffers only via bin levels
+# the bin levels must be sorted (agnostic to ascending or descending sort)
+# the levels are 0 indexed by default
+# each partition in the replay buffer stores training samples that were trained on a specific learning rate
+
+from queue import Queue
+# this is a fifo queue by default
+
+# TODO
+# do we need a parameter shuffle which can be toggled if training samples should be shuffled in the queue?
+class BucketizedReplayBuffer():
+
+    def __init__(self, number_of_buckets, data_source, max_bucket_size=0, start_state=0):
+        
+        self.queues = [Queue(max_bucket_size) for _ in range(number_of_buckets)]
+        self.number_of_buckets = number_of_buckets
+        self._source = data_source
+        self.lr_bucket = start_state
+        # some python magic
+        # self._add_lambdas = [(lambda batch, b=i: self.add_batch(batch, b)) for i in range(number_of_buckets)]
+
+        # some code for helping choose hyperparameters
+        # self._max_queue_size_global = 0
+        # self._max_queue_size =  (0, None)
+        # self._min_queue_size = (max_bucket_size+1, None)
+        # self._biggest_difference = 0
+
+    @property
+    def safe_to_sample(self):
+        return self.main.safe_to_sample
+
+    # def analytics(self, bucket):
+    #     # uncomment return to disable analytics
+    #     # return
+
+    #     # potential for walrus op
+    #     temp = self.queues[bucket].qsize()
+    #     if temp > self._max_queue_size_global:
+    #         self._max_queue_size_global = temp
+        
+    #     # will add more analytics if we need them
+    #     # dont want to overengineer!
+    #     # if self._max_queue_size[1] == bucket
+
+
+    # def add_batch(self, batch, bucket):
+
+    #     # uncomment below lines if you want your code to be slow but monkeyproof
+    #     # if bucket >= self.number_of_buckets:
+    #     #     raise(IndexError)
+        
+    #     # add to bucket
+    #     self.queues[bucket].put_nowait(batch)
+    #     # add to mirror bucket
+    #     self.queues[self.number_of_buckets - bucket - 1].put_nowait(batch)
+    def put(self, item):
+        self._source.put(item)
+
+    def get_batch(self, batch_size, prefetch_k=5):
+
+        if self.buckets[self.lr_bucket].qsize()==0:
+            new_batches = [self.main.get(batch_size) for _ in range(prefetch_k)]
+
+            [self.buckets[self.buckets -1 - self.lr_bucket].put_nowait(batch) for batch in new_batches]
+            [self.buckets[self.lr_bucket].put_nowait(batch) for batch in new_batches[1:]]
+            return new_batches[0]
+        else:
+            return self.buckets[self.lr_bucket].get()
+
+    
--- melissa4py/melissa4py/learn.py
+++ melissa4py/melissa4py/learn.py
@@ -7,7 +7,7 @@ import horovod.keras as hvd
 
 from collections import defaultdict
 from random import choice, sample
-from melissa4py.buffer import ReplayBuffer
+from melissa4py.buffer import ReplayBuffer, BucketizedReplayBuffer
 
 
 def build_lr_shedule(decrease_every=200, factor=0.5, min_lr=1e-6):
@@ -23,7 +23,7 @@ class BaseLearner:
     def __init__(self, batch_size=32, lr_start=0.001, lr_decrease_every=1,
                  lr_decrease_factor=1, lr_min_lr=1e-6, checkpoint_path='.',
                  checkpoint_every=20, lr_schedule=None, learn_every=None,
-                 replay_buffer=None, *args, **kwargs):
+                 replay_buffer=None, callbacks=[], *args, **kwargs):
         self.checkpoint_every = checkpoint_every
         self.checkpoint_path = checkpoint_path
         self.batch_size = batch_size
@@ -55,11 +55,12 @@ class BaseLearner:
             min_lr=lr_min_lr
         )
         # Setup callbacks
-        self.callbacks = [
-            tf.keras.callbacks.LearningRateScheduler(lr_schedule),
-        ]
-        if kwargs.get('ReduceLROnPlateau', None):
-            self.callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(**kwargs.get('ReduceLROnPlateau', None)))
+        self.callbacks = callbacks
+        # self.callbacks = [
+        #     # tf.keras.callbacks.LearningRateScheduler(lr_schedule),
+        # ]
+        # if kwargs.get('ReduceLROnPlateau', None):
+        #     self.callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(**kwargs.get('ReduceLROnPlateau', None)))
         # Set model
         for callback in self.callbacks:
             callback.set_model(self.model)
@@ -72,11 +73,14 @@ class BaseLearner:
     def learn(self, x, y):
         self.samples_seen += 1
         # Add sample to buffer
+        # partition here is the timestep (last index of x)
         self._buffer.put((x, y), partition=x[-1])
         if self._buffer.safe_to_sample and ((self.samples_seen % self.learn_every) == 0):
+            # interface level buffer here
+            # batch = get_batch_from_queue
             batch = self.samples_seen // self.batch_size
             # 1. Build batch.
-            samples = self._buffer.get_batch(self.batch_size)
+            samples = self._buffer.get_batch(self.batch_size, batch)
             xs, ys = [e[0] for e in samples], [e[1] for e in samples]
             X, Y = np.array(xs), np.array(ys)
             print('Batch timesteps: ', X[:, -1])
@@ -96,13 +100,34 @@ class BaseLearner:
             self.history['score'].append(score)
             self.history['learning_rate'].append(
                 tf.keras.backend.get_value(self.hvd_optimizer.lr)
+                # @Juan 
+                # this works then? 
+                # i thought we needed hvd.K.get_value() but they are both pointing to
+                # the same piece of code so great
+                # however I am using hvd.K instead of tf.keras.backend
             )
             # 4. Checkpoint model if necesary
             # if batch % self.checkpoint_every == 0:
             #     self.checkpoint(batch)
             return score
+    
+    def get_batch_for_queue(self):
+
+        bucket = self.schedule.get_bucket(self.hvd_optimizer.lr)
+        self.buffer.get_batch()
+        # get learning rate
+
+        # get bucket
+        # from learning rate scheduler
+        # self.lr_scheduler
+        #
+        self.source.get_batch(bucket)
 
     def on_batch_end(self, batch, score, samples):
+        
+        # get learning rate
+        # 
+
         pass
 
     def checkpoint(self, checkpoint_id):
@@ -123,3 +148,234 @@ class BaseLearner:
         self.model.build((1, self.nb_parameters))
         path = dirname+"/rank"+str(MPI.COMM_WORLD.Get_rank())+"/"+filename
         self.model.load_weights(path)
+
+
+# @Juan I couldn't find the learner.py in this repo, there is something messed up about
+# submodules
+# I think its something only in your submodule and you might have to push your latest changes, don't know
+class BucketLearner(BaseLearner):
+
+    def __init__(self, buckets):
+        
+        self.buckets = buckets
+        self.bucket_callback = Bucket_LR_Scheduler(0.01, 10**-5, buckets)
+        self.bucket_buffer = BucketizedReplayBuffer(buckets, ReplayBuffer())
+
+        super(lr_start=0.01,
+              replay_buffer=self.bucket_buffer,
+              callbacks=[self.bucket_callback]
+        )
+        
+    def build_model(self, *args, **kwargs):
+        # Compute neighborhood for a regular grid
+        nb_parameters = len(melissa_options.PARAMETERS) + 1
+        nb_fields = melissa_options.NB_FIELDS
+        grid_dims = melissa_options.GRID_DIMS
+        order = melissa_options.HYPERPARAMETERS['pixel_connectivity_order']
+        neighborhoods = regular_grid_neighbourhood(grid_dims, order=order)
+        neighborhoods = extend_neighbourhood(neighborhoods, nb_fields)
+        Neighboors.set_neighbours(neighborhoods)
+        model = MyModel(nb_parameters, np.prod(grid_dims) * nb_fields,
+                        **melissa_options.HYPERPARAMETERS)
+        return model
+
+    def get_batch_for_queue(self):
+        bucket = self.bucket_callback.get_bucket(self.hvd_optimizer.lr)
+        self.buffer.get_batch()
+        # get learning rate
+
+        # get bucket
+        # from learning rate scheduler
+        # self.lr_scheduler
+        #
+        self.source.get_batch(bucket)
+
+    def on_batch_end(self, batch, score, samples):
+        
+        # get learning rate
+        # 
+
+        pass
+
+
+
+# custom callback
+
+class Bucket_LR_Scheduler(Callback):
+
+    def __init__(self,
+                 maxlr,
+                 minlr,
+                 buckets,
+                 epochs_per_bucket=100,
+                 patience=100,
+                 monitor='mae',
+                 restore_best_weights=True,
+                 init_bucket=0):
+        
+        super(Bucket_LR_Scheduler, self).__init__()
+
+        # self.lr_min = minlr
+        self.init_bucket = init_bucket
+        # self.lr_max = maxlr
+        self.lrs = np.linspace(minlr, maxlr, buckets)
+        self.lr = self.lrs[init_bucket]
+        self.buckets = buckets
+        self.reverse_lrs = {(self.lrs[i],i) for i in range(buckets)}
+
+        # TODO use this
+        self.epochs_per_bucket = 100
+
+
+        self.monitor = monitor
+        self.baseline = None 
+        self.best = np.Inf
+        self.patience = patience
+
+        # we are not computing self.cycles but if we need to
+        # increment this variable in on_cycle_end method
+        self.cycles = 0
+        
+        # automatic early stopping with global patience
+        # number of epochs should enough for one complete cycle 
+        
+        self.global_patience = 10
+        self.cycles_waited = 0
+        # if we see no decrease in learning rate for 10 cycles
+        # we start searching for the local minima
+        # this is not a hyper parameter so that the user is not confused about the
+        # two patience variables
+        self.is_final_descent = False
+        self.final_descend = tf.keras.callbacks.ReduceLROnPlateau(
+            monitor='mae', 
+            factor=0.67, 
+            patience=100, 
+            verbose=0, 
+            mode='auto',
+            min_delta=0.0001, 
+            cooldown=0, 
+            min_lr=10**-7
+        )
+        # what should global patience help us with?
+        # when to start search for the local optima
+
+        self.min_delta = 0
+        self.wait = 0
+        
+        self.stopped_epoch = 0
+        self.restore_best_weights = restore_best_weights
+        self.best_weights = None
+        self.monitor_op = np.less
+
+        # counts how many buckets have been iterated till now
+        self.buckets_travelled = 0
+        self.cycles_waited = -1
+
+        self.epochs_in_bucket = 0
+        
+        def get_bucket(self, lr):
+            return self.reverse_lrs(lr)
+
+        def get_lr(self, lr):
+            self.buckets_travelled += 1
+            self.epochs_in_bucket = 0
+            return self.lrs[(self.get_bucket(lr) + 1)%self.buckets]
+
+        def schedule(self, epoch):
+            
+            # which bucket am I on?
+
+            # this logic needs to be tied to current_bucket 
+            # bucket = (self.init_bucket + epoch)%self.buckets
+            # the above line of code will not work as buckets are not tied to epochs
+            bucket = self.init_bucket + self.buckets_travelled
+            return self.lrs[bucket]
+        
+        def on_train_begin(self, logs=None):
+            # if we need to allow instances to be re-used
+            # use arguments to update members here
+            return None
+
+            # self.wait = 0
+            # self.stopped_epoch = 0
+            # self.best = np.Inf if self.monitor_op == np.less else -np.Inf
+
+        def on_cycle_end(self, epoch, logs):
+            
+            self.cycles_waited += 1
+            if self.cycles_waited > self.global_patience:
+                # set learning rate to max minimum as ReduceLROnPla.. doesnt have bestweights concept
+                # maybe it is a good idea to implement RLRoP here separately
+                hvd.K.set_value(self.model.optimizer.lr, self.lrs[0])
+                self.is_final_descent = True
+
+            return None
+        def on_epoch_end(self, epoch, logs=None):
+            
+            # if we have switched our learning rate scheduler mechanism
+            # from cyclic to final descent which is extreme decay to squeeze out best local minima
+
+            if self.is_final_descent:
+                # I don't know if this callback will be able to access the model
+                # will this work? :thinks:
+                self.final_descend.set_model(self.model)
+                # if it doesnt work we basically have to implement reduceLRonPlateau here
+                return self.final_descend.on_epoch_end(epoch, logs)
+            
+            # current mae loss
+            current = logs.get(self.monitor)
+
+            # How do we measure cycles?
+            # how many epochs make a cycle?
+            # it depends on the number of buckets iterated
+            # 2*buckets -1
+            # so we need to keep track of the number of buckets traversed
+            # calculation epochs would be an incorrect measure because of earlyUpdate
+            
+            if self.buckets_travelled % (2*self.buckets -1) == 0:
+                self.on_cycle_end(epoch, logs)
+            
+            # this condition will only be true if we forget to measure mae or changed our loss function
+            if current is None:
+                print("Monitoring Mean Absolute Error but mae not found in logs")
+                return
+
+            # if current mae is better than the previous best
+            if self.monitor_op(current - self.min_delta, self.best):
+                # update previous best
+                self.best = current
+                # reset wait counters
+                self.cycles_waited = 0
+                self.wait = 0
+                # update best weights
+                if self.restore_best_weights:
+                    self.best_weights = self.model.get_weights()
+                
+                if self.epochs_in_bucket == self.epochs_per_bucket:
+                    hvd.K.set_value(self.model.optimizer.lr, self.get_lr(self.model.optimizer.lr))
+            else:
+                # increment wait counter (for epoch here, for cycle is done in the on_cycle_end method)
+                self.wait += 1
+
+                # if we run out of patience (same logic for cycle patience in on_cycle_end)
+                if self.wait >= self.patience:
+                    
+                    # get the next cyclic learning rate
+                    hvd.K.set_value(self.model.optimizer.lr, self.get_lr(self.model.optimizer.lr))
+                    
+                    # restore best weights achieved in the previous cycle
+                    # I don't know how this works in horovod, will it involve
+                    # a lot of communication
+                    # does each server contain a reference to the best_weights?
+
+                    if self.restore_best_weights:
+                        print('Restoring model weights from ' + str(self.wait) + ' epochs before')
+                        self.model.set_weights(self.best_weights)
+
+                    # reset wait counter
+                    self.wait = 0
+
+        def on_train_end(self, logs=None):
+            return None
+            # if self.stopped_epoch > 0 and self.verbose > 0:
+            #     print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
