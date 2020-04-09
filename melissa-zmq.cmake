9dedc694d0f9ffb6c4c7dae4c1c92495bc0cfdec
sfriedem <sebastian.friedemann@inria.fr>
2020-04-09 16:13:36 +0200

    Add ZeroMQ_ROOT serach path

--- melissa-zmq.cmake
+++ melissa-zmq.cmake
@@ -27,13 +27,13 @@ else(INSTALL_ZMQ)
   ## use the hint from above to find where 'zmq.hpp' is located
   find_path(ZeroMQ_INCLUDE_DIR
           NAMES zmq.h
-          PATHS ${PC_ZeroMQ_INCLUDE_DIRS}
+          PATHS ${PC_ZeroMQ_INCLUDE_DIRS} "${ZeroMQ_ROOT}/include"
           )
 
   ## use the hint from about to find the location of libzmq
   find_library(ZeroMQ_LIBRARY
           NAMES zmq
-          PATHS ${PC_ZeroMQ_LIBRARY_DIRS}
+          PATHS ${PC_ZeroMQ_LIBRARY_DIRS} "${ZeroMQ_ROOT}/lib"
           )
   # TODO: ensure that zmq is the correct version!
 endif(INSTALL_ZMQ)
