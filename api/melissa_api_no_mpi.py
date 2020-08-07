d6c9fdf64c20d78d81d09f782cd429d0c8864f58
Christoph Conrads <christoph.conrads@inria.fr>
2020-08-07 14:52:49 +0200

    Fix spelling of "LICENSE"

--- api/melissa_api_no_mpi.py
+++ api/melissa_api_no_mpi.py
@@ -6,7 +6,7 @@
 #   COPYRIGHT (C) 2017  by INRIA and EDF. ALL RIGHTS RESERVED.    #
 #                                                                 #
 # This source is covered by the BSD 3-Clause License.             #
-# Refer to the  LICENCE file for further information.             #
+# Refer to the  LICENSE file for further information.             #
 #                                                                 #
 #-----------------------------------------------------------------#
 #  Original Contributors:                                         #
