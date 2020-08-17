a348a508046d4b5b0e9f201f5547709072124851
Christoph Conrads <christoph.conrads@inria.fr>
2020-08-17 16:21:33 +0200

    CMake: let standard module handle version checking

--- cmake/Modules/FindZeroMQ.cmake
+++ cmake/Modules/FindZeroMQ.cmake
@@ -51,20 +51,6 @@ endif()
 
 
 #
-# check pkg-config results
-#
-if(NOT _PC_ZeroMQ_FOUND)
-    message(FATAL_ERROR "Could NOT find ZeroMQ")
-endif()
-
-set(_ZeroMQ_MINIMUM_REQUIRED_VERSION 3.0)
-
-if(_PC_ZeroMQ_VERSION VERSION_LESS _ZeroMQ_MINIMUM_REQUIRED_VERSION)
-    message(FATAL_ERROR "ZeroMQ ${_ZeroMQ_MINIMUM_REQUIRED_VERSION} or newer required")
-endif()
-
-
-#
 # set variables for caller
 # suppress default paths to ensure the software found by pkg-config is used
 #
@@ -78,12 +64,14 @@ find_library(ZeroMQ_LIBRARY
              PATHS ${_PC_ZeroMQ_LIBRARY_DIRS}
              NO_DEFAULT_PATH)
 
-set(ZeroMQ_VERSION ${_PC_ZeroMQ_VERSION} CACHE INTERNAL "ZeroMQ version")
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ZeroMQ
                                   FOUND_VAR ZeroMQ_FOUND
                                   REQUIRED_VARS ZeroMQ_INCLUDE_DIR ZeroMQ_LIBRARY
                                   VERSION_VAR _PC_ZeroMQ_VERSION)
+
+if(ZeroMQ_FOUND)
+    set(ZeroMQ_VERSION ${_PC_ZeroMQ_VERSION} CACHE INTERNAL "ZeroMQ version")
+endif()
 
 mark_as_advanced(ZeroMQ_INCLUDE_DIR ZeroMQ_LIBRARY)
