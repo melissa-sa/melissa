54318900b5840699d268029d7570d60e663b3d64
Christoph Conrads <christoph.conrads@inria.fr>
2020-11-25 19:23:42 +0000

    CMake: add ZeroMQ 4.3.1 tarball hash
    Add the SHA256 hash because the hash
    * stops CMake from downloading the file on every invocation,
    * provides a bit of defense in depth.

--- cmake/Modules/BuildZeroMQ.cmake
+++ cmake/Modules/BuildZeroMQ.cmake
@@ -15,6 +15,7 @@ if(INSTALL_ZMQ)
     ExternalProject_Add(
         ZeroMQ
         URL https://github.com/zeromq/libzmq/releases/download/v4.3.1/zeromq-4.3.1.tar.gz
+        URL_HASH SHA256=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/ZeroMQ
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_INSTALL_LIBDIR:PATH=${CMAKE_INSTALL_PREFIX}/lib
