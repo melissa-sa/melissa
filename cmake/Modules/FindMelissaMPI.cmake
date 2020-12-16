8d6ec7d04413a3d35b7971cd2a70aa8d3f01cd6e
Christoph Conrads <christoph.conrads@inria.fr>
2020-12-16 16:24:20 +0000

    CMake: add missing license boilerplate

--- cmake/Modules/FindMelissaMPI.cmake
+++ cmake/Modules/FindMelissaMPI.cmake
@@ -1,3 +1,33 @@
+# Copyright (c) 2020, Institut National de Recherche en Informatique et en Automatique (https://www.inria.fr/)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 find_package(MPI)
 
 foreach(_language IN ITEMS C CXX Fortran)
@@ -9,7 +39,7 @@ foreach(_language IN ITEMS C CXX Fortran)
         message(SEND_FATAL "MPI_${_language} not found")
     endif()
 
-    # fix compilation on CentOS 8 with CMake 3.11
+    # fix compilation on CentOS 8 with CMake 3.11.4, OpenMPI 4.0.2
     separate_arguments(MPI_${_language}_COMPILE_FLAGS)
 
     # remove leading, trailing whitespace when CMake 3.7 + MPICH is used
