1739c61669a87a5c1aac1bdaa699e45fbeb6694d
Christoph Conrads <christoph.conrads@inria.fr>
2020-12-24 18:19:36 +0000

    Tests: make dummy simulation fully deterministic
    This commit makes it trivial to check the computed statistics for
    correctness.

--- tests/dummy-simulation.c
+++ tests/dummy-simulation.c
@@ -31,79 +31,35 @@
 #include <melissa/api.h>
 
 #include <assert.h>
-#include <inttypes.h>
-#include <limits.h>
 #include <math.h>
+#include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
 int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 
-	if(argc < 2)
-	{
-		const char* executable = (argc > 0) ? argv[0] : "a.out";
-		fprintf(stderr, "usage: %s <seed>\n", executable);
-		return EXIT_FAILURE;
-	}
+    const char* simu_id_str = getenv("MELISSA_SIMU_ID");
 
+    if(!simu_id_str) {
+        exit(EXIT_FAILURE);
+    }
 
-	unsigned short prng_state[3] = { 0 };
-	// parse command line
-	{
-		size_t len = strlen(argv[1]);
-		// check if the caller casted the int data to float
-		if(argv[1][len-2] == '.') {
-			argv[1][len-2] = '\0';
-		}
-
-		char* first = argv[1];
-		char* last = NULL;
-		int base = 0;
-		// use a signed value because strtoull does not return errors when
-		// parsing negative values
-		long long raw_seed = strtoll(first, &last, base);
-
-		assert(last);
-
-		if(first == last || *last != '\0') {
-			fprintf(
-				stderr,
-				"cannot parse seed '%s' as integer\n", first
-			);
-			exit(EXIT_FAILURE);
-		}
-
-		const int64_t SEED_MAX = (INT64_C(1) << 48) - 1;
-
-		if(raw_seed < 0 || raw_seed > SEED_MAX) {
-			fprintf(
-				stderr,
-				"expected seed in interval [0, 2**48), got %s\n",
-				first
-			);
-			exit(EXIT_FAILURE);
-		}
-
-		uint64_t seed = raw_seed;
-		memcpy(&prng_state, &seed, sizeof(prng_state));
-	}
-
+    int sid = atoi(simu_id_str);
+	const unsigned num_iterations = 2;
 	const char field_name[] = "heat1";
 	const size_t vector_size = 2;
 	double data[2] = { 0, NAN };
 
 	melissa_init(field_name, vector_size, MPI_COMM_WORLD);
 
-	const unsigned num_iterations = 2;
 	for(unsigned it = 0; it < num_iterations; ++it) {
-		data[1] = erand48(prng_state) - 0.5;
+		data[1] = 1.0 * it * sid;
 
 		melissa_send(field_name, data);
 		assert(data[0] == 0);
+		assert(data[1] == 1.0 * it * sid);
 	}
 
 	melissa_finalize();
